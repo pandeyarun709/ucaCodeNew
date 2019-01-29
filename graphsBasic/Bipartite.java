276e042857d9027a0f4f1f3ad0b3f1aeb13e6961
pandeyarun709 <pandeyarun709@gmail.com>
2019-01-29 23:20:46 +0530

    Bipartite

---
+++ graphsBasic/Bipartite.java
@@ -0,0 +1,76 @@
+package graphsBasic;
+
+import java.util.*;
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/*Complete the function below*/
+class Graph{
+     int V;
+     LinkedList<Integer> adj [] ;
+    
+    public Graph(int V){
+        this.V = V;
+        adj = new LinkedList[V];
+        for(int i=0;i<V;i++)
+            adj[i] = new LinkedList<>();
+    }
+    
+    public void addEdge(int u , int v){
+        adj[u].add(v);
+    }
+
+}
+
+
+public class Bipartite {
+	
+      public boolean dfs(Graph g,boolean []visited,boolean []color,int v)
+      {
+          for(int w : g.adj[v])
+          {
+              if(!visited[w]){
+                 visited[w] = true;
+                 color[w] = !color[v];
+                 
+                 if(!dfs(g, visited,color,w))
+                      return false;
+              }else if(color[v] == color[w])
+                      return false;
+          } 
+          return true;
+      }
+    
+    
+    
+	  boolean isBipartite(int G[][],int V)
+       {
+          //add code here.
+          Graph g = new Graph(V);
+          
+          for(int i=0;i<V ;i++)
+          {
+              for(int j = 0;j<V;j++)
+              {
+                  if(G[i][j] == 1)
+                     g.addEdge(i,j);
+              }
+          }
+          boolean []visited = new boolean[V];
+           boolean []col = new boolean[V];
+          Arrays.fill(visited , false);
+          
+          for(int i=0;i<V;i++)
+          {
+              if(!visited[i]){
+                  visited[i] = true;
+                  col[i] = true;
+                  if(!dfs(g,visited,col,i))
+                   return false;
+              }
+          }   
+          return true;
+        }
+}
