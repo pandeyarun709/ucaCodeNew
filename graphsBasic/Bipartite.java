eb13584f5209f5c1a411044e6a016515b4a82cdf
pandeyarun709 <pandeyarun709@gmail.com>
2019-01-29 23:25:23 +0530

    bipartite

--- graphsBasic/Bipartite.java
+++ graphsBasic/Bipartite.java
@@ -2,11 +2,7 @@ package graphsBasic;
 
 import java.util.*;
 
-/*Please note that it's Function problem i.e.
-you need to write your solution in the form of Function(s) only.
-Driver Code to call/invoke your function is mentioned above.*/
 
-/*Complete the function below*/
 class Graph{
      int V;
      LinkedList<Integer> adj [] ;
