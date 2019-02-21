00a4ec4cb8c4ed9266a966c07c1ecd3b0234f704
pandeyarun709 <pandeyarun709@gmail.com>
2019-02-21 21:58:56 +0530

    Avl tree

--- com/uca/ds/Tree/avl/AVL.java
+++ com/uca/ds/Tree/avl/AVL.java
@@ -1,26 +1,103 @@
 package com.uca.ds.Tree.avl;
 
-public class AVL {
-	
-	//private
-	
+public class AVL {    
+		    
+		    public Node insertToAVL(Node node,int data)
+		    {
+		         //add code here.
+		         if(node == null)
+		           return new Node(data);
+		           
+		         if(node.data < data )
+		           node.left = insertToAVL(node.left , data);
+		           
+		         else if(node.data > data)
+		              node.right = insertToAVL(node.right, data);
+		         
+		         else return node; //if node already in tree
+		         
+		         //calculate height 
+		         node.height = 1 + Math.max(getHeight(node.left) , getHeight(node.right)); 
+		         
+		         int bal = getHeight(node.left) - getHeight(node.right); 
+		         
+		         
+		         /// ##################dataAVL balancing ################################
+		         
+		         if(bal > 1 && data < node.left.data) // new node added to left side (Left Left Case) --> right rotate
+		         {
+		             return rightRotate(node);
+		         }
+		         
+		         if(bal > 1 && data > node.left.data) //// new node added to right side of child  (Left right Case) --> right rotate
+		         {
+		             node.left = leftRotate(node.left);
+		             
+		             return rightRotate(node);
+		         }
+		         
+		         //------------- we height of right sub tree more -------------------
+		         
+		        if(bal < -1 && data > node.right.data) // new node added to right side (right right Case) --> left rotate
+		         {
+		             return leftRotate(node);
+		         }
+		         
+		         if(bal < -1 && data < node.right.data) //// new node added to left side of child  (right left Case) --> right rotate
+		         {
+		             node.left = rightRotate(node.left);
+		             
+		             return leftRotate(node);
+		         }
+		         
+		        return node; 
+		    }
+		    
+			// RIght rotate------------------------------------------------
+			   private Node rightRotate(Node node)
+			   {
+			       Node root = node.left;
+			       Node temp = root.right;
+			       
+			       root.right = node;
+			       node.left = temp; 
+			       
+			       return root;
+			    
+			   }
+			   
+			   // Left Rotate --------------------------------------
+			   private Node leftRotate(Node node)
+			   {
+			        Node root = node.right;
+			       Node temp = node.right.left; // node right left(right going to be root node)
+			       
+			       root.left = node;
+			       node.right = temp; 
+			       
+			       return root;
+			   }
 
-	
-	
-	
+
+			    public int getHeight(Node n)
+			    {
+			        return n.height;
+			    }
+		
 	
 	
 	
 /*######### Node class ####################*/	
 	private class Node{
-		private int key;
+		private int data;
 		private Node left;
 		private Node right;
-		
-		public Node(int key) {
-			this.key = key;
+		private int height;
+		public Node(int data) {
+			this.data = data;
 			left = null;
 			right = null;
+			height = 0;
 		}
 	}
 }
